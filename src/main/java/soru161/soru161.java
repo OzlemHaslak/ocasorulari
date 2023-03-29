7a5e4cd3f0f74c92ed7cd5f862d1bbef46133800
OzlemHaslak <ozlemhaslak@gmail.com>
2023-03-29 21:23:32 +0200

    v6

---
+++ src/main/java/soru161/soru161.java
@@ -0,0 +1,16 @@
+package soru161;
+
+public class soru161 {
+    public static void main(String[] args) {
+
+
+     //   Boolean available = "TRUE";
+
+      //  String tmpAuthor = author, author = "Mc Donald";
+
+        Double price = 200D;
+
+        Integer pages = 20;
+
+    }
+}
